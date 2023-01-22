edda34ad8af2945355917c088bd588bebc259e72
Serhii Kalmykov <serj122@gmail.com>
2023-01-22 10:20:08 +0200

    hw7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryCards = createGallery(galleryItems);
+
+galleryContainer.innerHTML = galleryCards;
+galleryContainer.addEventListener("click", onSmallImageClick);
+
+function createGallery(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+         <a class="gallery__link" href="${original}">
+         <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+         />
+        </a>
+        </div>`
+    )
+    .join("");
+}
+
+function onSmallImageClick(evt) {
+  evt.preventDefault();
+
+  const isImage = evt.target.classList.contains("gallery__image");
+  if (!isImage) {
+    return;
+  }
+
+  openModalBigImage(evt.target.dataset.source);
+}
+
+function openModalBigImage(url) {
+  const closeModal = (evt) => {
+    if (evt.code === "Escape") {
+      bigImage.close();
+    }
+  };
+
+  const bigImage = basicLightbox.create(
+    `
+		<img src="${url}">`,
+    {
+      onShow: (bigImage) => document.addEventListener("keydown", closeModal),
+      onClose: (bigImage) =>
+        document.removeEventListener("keydown", closeModal),
+    }
+  );
+  bigImage.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryCards = createGallery(galleryItems);
+
+galleryContainer.innerHTML = galleryCards;
+galleryContainer.addEventListener("click", onSmallImageClick);
+
+function createGallery(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+         <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+         />
+        </a>
+        `
+    )
+    .join("");
+}
+
+function onSmallImageClick(evt) {
+  evt.preventDefault();
+
+  const isImage = evt.target.classList.contains("gallery__image");
+  if (!isImage) {
+    return;
+  }
+  
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionDelay: 250,
+});
